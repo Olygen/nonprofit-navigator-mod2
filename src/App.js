e530750a427302b12872ac530406d05a668084ca
Lauren <laurenc2335@gmail.com>
2023-04-19 22:24:21 -0500

    seventh commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
+  const center = useMemo(() => ({ lat: 32.779167, lng: -96.808891 }), []);
+
+  return (
+    <div className="App">
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+          mapContainerClassName="map-container"
+          center={center}
+          zoom={10}
+        >
+          <Marker position={{ lat: 32.779167, lng: -96.808891 }} />
+        </GoogleMap>
+      )}
+    </div>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Nonprofit Navigator</h1>
       </header>
     </div>
   );
-}
-
+  }
 export default App;
